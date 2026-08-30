2ac403521ab4d027a8a0b40a8d0921ab3fb8ae3c
ykm <y.kimata.mic@gmail.com>
2026-08-30 17:06:22 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/trick-card-games/',
   define: {
     __APP_VERSION__: JSON.stringify(new Date().toISOString().slice(0, 10)),
   },
